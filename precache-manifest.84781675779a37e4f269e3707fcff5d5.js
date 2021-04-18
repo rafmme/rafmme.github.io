9970da3e0ea803ad097a47e4e6fc901dae5522a7
Timileyin Farayola <rafmme@gmail.com>
2021-04-18 11:36:06 +0100

    Updates

---
+++ precache-manifest.84781675779a37e4f269e3707fcff5d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28e8daa757dd8b9dce73ff0e5549adf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a429d4754421bf23ee5",
+    "url": "/static/js/2.42e65345.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.42e65345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d86ca0c9da8e66ebc3c",
+    "url": "/static/js/main.8ff78f43.chunk.js"
+  },
+  {
+    "revision": "b3763a5d5cf1f4a4fa9c",
+    "url": "/static/js/runtime-main.b7cbe337.js"
+  }
+]);
